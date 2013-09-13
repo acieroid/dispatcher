4da6b459d26bc44e52fe00f0bf10db1217025eb3
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-09-13 17:12:42 +0200

    Sketch interface of dispatcher

--- src/dispatcher/core.clj
+++ src/dispatcher/core.clj
@@ -1,6 +1,21 @@
 (ns dispatcher.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Dispatcher [destination throughput expected buffer])
+
+(defn create
+  "Create a dispatcher and launch a background task that will send the
+  messages without exceeding the given throughput."
+  [destination throughput]
+  (let [dispatcher (Dispatcher.)])
+  TODO)
+
+(defn enqueue
+  "Enqueue an event to be sent later"
+  [dispatcher event]
+  TODO)
+
+(defn expect
+  "Remembers that an event is expected to happen now. Will compute the
+  delay between now and the time it will really happen"
+  [dispatcher event]
+  TODO)
