4e418d7f31292c2af62df1af2bc14a94d2bf0241
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-09-13 16:14:23 +0200

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject dispatcher "0.0.1-SNAPSHOT"
+  :description "Send events to a zmq server with a given throughput"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
