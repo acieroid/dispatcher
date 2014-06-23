1463cc7696d0587bda0b4139c7eda84e5b092cf5
Gary Verhaegen <gary.verhaegen@gmail.com>
2014-06-23 14:12:56 +0200

    Remove unused Sonatype repo

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [naughtmq "0.0.2"]
-                 [org.zeromq/jzmq "2.2.2"]]
-  :repositories {"sonatype-nexus-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"})
+                 [org.zeromq/jzmq "2.2.2"]])
